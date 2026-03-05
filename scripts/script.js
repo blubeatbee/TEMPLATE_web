479dc15a252a171fc57abb77dcdbe446b67b28b9
Alex <alexcoh19@outlook.com>
2026-03-05 10:17:06 +0100

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+const colors = ['red', 'green', 'blue'];
+
+let user = {
+	userName: "Aldor",
+	age: 30,
+	isTeacher: true
+};
+
+let today = new Date();
+
+console.log("Hello World!");
+console.log(colors[0], colors[1], colors[2]);
+console.log(user.userName);
+console.log(today);
